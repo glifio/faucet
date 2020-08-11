b2cb4de835f6d93107e9802d5a3015359a84b17e
Jon <Schwartz10@users.noreply.github.com>
2020-08-11 09:12:12 -0400

    Update CardStates.jsx

--- components/PostAuth/CardStates.jsx
+++ components/PostAuth/CardStates.jsx
@@ -8,7 +8,7 @@ export const Confirming = ({ cid, err }) => {
     <>
       <Text>
         We're making sure your FIL request gets processed by the Filecoin
-        network. This might take 2-3 minutes.
+        network. This might take up to 5 minutes.
       </Text>
       <Box
         display='flex'
