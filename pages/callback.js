b9bffb1f53d19506f1311edc1813640bb83de9c3
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-08-11 09:10:50 -0400

    Do not browser cache redirect

--- pages/callback.js
+++ pages/callback.js
@@ -22,7 +22,7 @@ export default class Callback extends Component {
         // we redirect the error in the catch statement,
         // since reportError can only make client side transitions
         reportError('pages/callback.js:1', false, err.message, err.stack)
-        res.writeHead(301, { Location: '/error' })
+        res.writeHead(307, { Location: '/error' })
         res.end()
       } else {
         reportError('pages/callback.js:1', true, err.message, err.stack)
